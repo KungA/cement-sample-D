82b488065bf9ac177acb890321883db28a3bd446
kungurtsev <kungurtsev@skbkontur.ru>
2017-06-20 13:45:08 +0500

    init

---
+++ D/ClassD.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D
+{
+    public class ClassD
+    {
+        public void Run()
+        {
+            Console.WriteLine("    D class");
+        }
+    }
+}
